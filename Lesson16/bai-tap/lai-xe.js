6ea70089401b2727aeb8c64d8193118779ea43db
Cuongrevival <trainingsession495@gmail.com>
2025-01-24 11:14:03 +0700

    Bài tập thực hành Lesson16

--- Lesson16/bai-tap/lai-xe.js
+++ Lesson16/bai-tap/lai-xe.js
@@ -4,9 +4,11 @@ let isGameOver = false;
 let car = {
     x: 150,
     y: 400,
-    width: 50,
-    height: 50
+    width: 40,
+    height: 100,
+    image: new Image()
 }
+car.image.src = 'car1.jpg';
 let obstacles = [
     {x: 100, y: 200, width: 20, height: 20},
     {x: 200, y: 100, width: 20, height: 20},
@@ -19,8 +21,7 @@ function start() {
         return;
     }
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    ctx.fillStyle = "red";
-    ctx.fillRect(car.x, car.y, car.width, car.height);
+    ctx.drawImage(car.image, car.x, car.y, car.width, car.height);
     touchCanvas();
     ctx.fillStyle = "green";
     obstacles.forEach(obstacle => {
@@ -41,13 +42,13 @@ document.addEventListener('keydown', key => {
                 car.y -= 5;
             break;
         case 39:
-            if (car.x + 50 <= 400) {
+            if (car.x + car.width < myCanvas.width) {
                 car.x += 5;
             }
             break;
 
         case 40:
-            if (car.y + 50 <= 600) {
+            if (car.y + car.height < myCanvas.height) {
                 car.y += 5;
                 break;
             }
@@ -55,7 +56,10 @@ document.addEventListener('keydown', key => {
 });
 
 function touchCanvas() {
-    if (car.x <= 0 || car.x + car.width >= 400 || car.y + car.height >= 600 || car.y <= 0) {
+    if ( car.x <= 0 ||
+        car.x + car.width >= myCanvas.width ||
+        car.y + car.height >= myCanvas.height ||
+        car.y <= 0) {
         isGameOver = true;
         alert('Bạn thua')
     }
@@ -63,12 +67,13 @@ function touchCanvas() {
 
 function touchObstacle() {
     obstacles.forEach(obstacle => {
-        if (car.x < obstacle.x + obstacle.width &&
+        if (car.x <obstacle.x + obstacle.width &&
             car.x + car.width > obstacle.x &&
             car.y < obstacle.y + obstacle.height &&
             car.y + car.height > obstacle.y) {
             isGameOver = true;
-            alert('Bạn thua');
+            alert('Bạn thua')
         }
+
     });
 }
